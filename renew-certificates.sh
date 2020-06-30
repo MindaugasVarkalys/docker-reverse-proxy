0d21d744aca9166a8e0ffffa2524d2c49f028a49
Mindaugas Varkalys <MindaugasVarkalys@users.noreply.github.com>
2020-06-30 15:09:13 +0300

    Remove interactive flag
    Remove interactive flag since Crontab is not interactive

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -2,7 +2,7 @@
 
 cd  "${BASH_SOURCE%/*}" || exit
 docker pull certbot/certbot
-docker run -it --rm -v "${PWD}/certbot:/etc/letsencrypt" -v "${PWD}/certbot/www:/var/www/certbot" certbot/certbot renew -n
+docker run --rm -v "${PWD}/certbot:/etc/letsencrypt" -v "${PWD}/certbot/www:/var/www/certbot" certbot/certbot renew -n
 docker-compose pull
 docker-compose stop
 docker-compose up -d
